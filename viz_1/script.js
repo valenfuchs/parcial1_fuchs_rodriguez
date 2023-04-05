8cb42a41bcea92fdf8d9b97f6a1c0b5007d0a01f
valen fuchs <fuchsvalen@gmail.com>
2023-04-05 11:46:09 -0300

    barras

--- viz_1/script.js
+++ viz_1/script.js
@@ -1,1 +1,19 @@
-x
+d3.dsv(';','147_15-21_junio.csv', d3.autoType).then(data => {
+  
+    console.log(data)
+  
+    let chart = Plot.plot({
+      marks: [
+        Plot.barY(data, Plot.groupX({y: 'count'},
+          {x: 'prestacion',
+          fill: d => (d.prestacion == 'Vehículo mal estacionado' ? 'rgb(255, 128, 0)' :  '#cfcfcf')},
+        )),
+        
+    ],
+
+    })
+
+    d3.select('#chart').append(() => chart)
+  })
+  
+  
