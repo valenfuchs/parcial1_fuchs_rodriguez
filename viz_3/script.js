132f47f36bd0fb24ab07d953cda0cd6bb798a2bb
valen fuchs <fuchsvalen@gmail.com>
2023-04-14 17:28:06 -0300

    .

--- viz_3/script.js
+++ viz_3/script.js
@@ -1,5 +1,6 @@
 d3.dsv(';','/data/147_vehiculos_mal_estacionados.csv', d3.autoType).then(data => {
 
+  data = data.filter(d => d.canal == 'App Denuncia Vial'||d.canal == 'GCS Web'||d.canal == 'Boti'||d.canal == 'App BA 147')
  
     let chart = Plot.plot({
         marks: [
@@ -14,20 +15,31 @@ d3.dsv(';','/data/147_vehiculos_mal_estacionados.csv', d3.autoType).then(data =>
               },
             ),
           ),
-          Plot.text(
+          
+          /*Plot.text(
             data,
             Plot.groupX(
-              {y: 'count', text: 'canal',},
+              {y: 'count'
+              //, text: 'canal',
+            },
               {
                 x:  d => d3.timeParse("%d/%m/%Y")(d.fecha_ingreso),
                 textAnchor: 'start',
                 dx: 5,
                 dy: -5,
-                fill: 'canal'
+                fill: 'canal',
               },
             ),
           ),
+          */
         ],
+
+        
+
+        color: {
+          scheme: 'bupu',
+          legend: true,
+        },
      
       })
     d3.select('#chart').append(() => chart)
